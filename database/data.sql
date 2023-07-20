570e0659b512b0a227b64e18ead3f82879f52729
Macy Hong <macyhong@gmail.com>
2023-07-20 08:09:49 +0000

    Set up databases

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,16 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+
+insert into "users" ("username", "email")
+values
+  ('moviewatcher', 'ilovemovies@email.com'),
+  ('heyarnold', 'footballhead@email.com'),
+  ('critic', 'ihatemovies@email.com');
+
+insert into "user_activity" ("userId", "movieId", "thumbsUp", "thumbsDown")
+VALUES
+  (3, 'rm2267976192', false, true),
+  (3, 'tt12210550', false, true),
+  (3, 'rm706774528', false, true);
+
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,39 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."users" (
+	"userId" serial NOT NULL,
+	"username" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."watch_list" (
+	"userId" integer NOT NULL,
+	"movieId" varchar(255) NOT NULL,
+	"watched" BOOLEAN NOT NULL,
+	CONSTRAINT "watch_list_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "public"."user_activity" (
+	"userId" integer NOT NULL,
+	"movieId" varchar(255) NOT NULL,
+	"thumbsUp" BOOLEAN NOT NULL,
+	"thumbsDown" BOOLEAN NOT NULL,
+	CONSTRAINT "user_activity_pk" PRIMARY KEY ("userId","movieId")
+) WITH (
+  OIDS=FALSE
+);
+
+-- will need to set up a database of created movie cards
+
+ALTER TABLE "watch_list" ADD CONSTRAINT "watch_list_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+
+ALTER TABLE "user_activity" ADD CONSTRAINT "user_activity_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
